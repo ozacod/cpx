ddb5c26eb42b69e2a5ceb4c85beec0124045b23c
ozacod <47009516+ozacod@users.noreply.github.com>
2025-12-10 22:59:14 +0300

    Bump version to 1.0.9

--- cpx/internal/app/cli/common.go
+++ cpx/internal/app/cli/common.go
@@ -28,7 +28,7 @@ const (
 )
 
 // Version is the cpx version
-const Version = "1.0.8"
+const Version = "1.0.9"
 
 // DefaultServer is the default server URL
 const DefaultServer = "https://cpx-dev.vercel.app"
